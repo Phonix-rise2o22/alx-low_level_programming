220311d35e3349105f080e1fa222b654dcf2ea28
Irene Mwaniki <irenemwaniki93@gmail.com>
2022-06-30 07:03:03 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,11 @@
 #!/bin/bash
-#include <stdio.h>
+#include<stdlo.h>
+
+int main(){
 
 
 
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
 
-int main(void)
-
-{
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }

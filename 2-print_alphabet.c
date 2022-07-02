7cae33befe15d143cfad5f20bf276a0ddd175f97
Irene Mwaniki <irenemwaniki93@gmail.com>
2022-07-01 17:13:10 -0700

    task 2

---
+++ 2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Begin here
+ * print alphabet a-z using putchar
+ * Return : always 0
+ */
+int main(void)
+{
+	int ch;
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar("\n");
+
+	return (0);
+}

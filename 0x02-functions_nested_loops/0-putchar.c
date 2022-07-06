81577af9660f09df089a8e4943ed264785dad878
Irene Mwaniki <irenemwaniki93@gmail.com>
2022-07-06 08:00:50 -0700

    task o

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,16 @@
  * main- Begin here
  * Return:always 0
  */
+
 int main(void)
 {
-	char text[] = "_putchar";
-	int i;
+char text[] = "_putchar";
+int i;
 
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
-	return (0);
+for (i = 0; i < 8; i++)
+{
+_putchar(text[i]);
+}
+_putchar('\n');
+return (0);
 }
